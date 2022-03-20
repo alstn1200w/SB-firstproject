f36903185ed03256203e26dd65ca037b3a1f1797
Minsoo <alstn1200w@naver.com>
2022-03-21 01:34:14 +0900

    first commit

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,20 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("hi")
+    public String niceToMeetTou(Model model){
+        model.addAttribute("username", "민수2 ");
+        return "greetings";
+    }
+    @GetMapping("bye")
+    public  String seeYouNext(Model model){
+        model.addAttribute("nickname", "민수5");
+        return "goodbye";
+    }
+}
