049959b5709a5d99941d4644356c8987b75e31d5
Minsoo <alstn1200w@naver.com>
2022-03-22 00:14:51 +0900

    자기전 커밋

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,14 +1,21 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity // 대표값을 지정! Like a 주민등록번호
+@Entity
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
 public class Article {
 
-    @Id
+    @Id // 대표값을 지정! Like a 주민등록번호
     @GeneratedValue // 1, 2, 3 ....자동 생성 어노테이션
     private Long id;
 
@@ -17,22 +24,4 @@ public class Article {
 
     @Column
     private String content;
-
-    public Article() {
-    }
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
