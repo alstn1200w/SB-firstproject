bb8e9222a59b2aaf033b736e1a7a89eb7e329a79
Minsoo <alstn1200w@naver.com>
2022-03-22 16:37:29 +0900

    퇴근 전 커밋

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 @AllArgsConstructor
 @ToString
 @NoArgsConstructor
+@Getter
 public class Article {
 
     @Id // 대표값을 지정! Like a 주민등록번호
